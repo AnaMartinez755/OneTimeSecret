d6e97277b5d0a9441c0d0d173de7b3217d67eeab
AnaMartinez775 <martinezcarballoana@gmail.com>
2024-03-12 21:39:57 -0400

    second commit

--- index.ts
+++ index.ts
@@ -8,33 +8,36 @@ const port = 5000;
 app.use(bodyParser.json());
 
 // Store in memory
-let secrets: { [key: string]: string } = {};
+const secrets_map: { [key: string]: string } = {};
 
-function generateSecretKey(): number {
-  const bytes = crypto.randomBytes(4);
-  const randomNumber = parseInt(bytes.toString("hex"), 16);
+function aleatoryValue(): number {
+  let randomNumber: number;
+  do {
+    const bytes = crypto.randomBytes(16);
+    randomNumber = parseInt(bytes.toString("hex"), 16);
+  } while (secrets_map[randomNumber] !== undefined); // Check if the number already exists
   return randomNumber;
 }
 
 app.post("/api/secrets", (req: Request, res: Response) => {
   const { message }: { message: string } = req.body;
   if (!message) {
-    return res.status(400).json({ error: "message is necesary" });
+    return res.status(400).json({ error: "message not send" });
   }
-  const secretKey: number = generateSecretKey();
-  secrets[secretKey] = message;
-  res.status(201).json({ secretKey });
+  const secretKey: number = aleatoryValue();
+  secrets_map[secretKey] = message;
+  res.status(201).json({ secretKey }); // in create --> created succesfull
 });
 
 app.get("/api/secret/:secretKey", (req: Request, res: Response) => {
   const { secretKey } = req.params as { secretKey: string };
-  const secretMessage: string | undefined = secrets[secretKey];
+  const secretMessage: string | undefined = secrets_map[secretKey];
   if (!secretMessage) {
     return res.sendStatus(404);
   }
   // Delete the second time
-  delete secrets[secretKey];
-  res.json({ secretMessage });
+  delete secrets_map[secretKey];
+  res.status(200).json({ secretMessage });
 });
 
 app.listen(port, () => {
