5bee5c133095977ecada254f88360c2f236ec997
AnaMartinez775 <martinezcarballoana@gmail.com>
2024-03-14 15:08:43 -0400

    coverage addition

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
   testEnvironment: "node",
   coveragePathIgnorePatterns: ["/node_modules/"],
 };
