efbbc22472e37504cfb79e8c7dc8396a7a02031b
AnaMartinez775 <martinezcarballoana@gmail.com>
2024-03-18 01:52:39 -0400

    two endpoints test

--- test/server.test.ts
+++ test/server.test.ts
@@ -19,3 +19,19 @@ describe("POST /api/secrets", () => {
     });
   });
 });
+
+describe("GET /api/secrets/:secretKey", () => {
+  describe("get a secret message", () => {
+    test("should retrieve a secret using the key returned from POST", async () => {
+      const createResponse = await request(app).post("/api/secrets").send({
+        message: "Hello world",
+      });
+      expect(createResponse.status).toBe(201);
+      const { secretKey } = await createResponse.body;
+      const getResponse = await request(app).get(`/api/secret/${secretKey}`);
+      expect(getResponse.status).toBe(200);
+      const parsedGetResponse = await getResponse.body;
+      expect(parsedGetResponse.secretMessage).toBe("Hello world");
+    });
+  });
+});
